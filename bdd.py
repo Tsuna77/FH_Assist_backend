02bb312365485601e121fabb6fed2dbd5b6871c8
Raphaël Ricordeau <raphael.ricordeau@ac-dijon.fr>
2019-06-27 22:27:09 +0200

    divers correctif

--- bdd.py
+++ bdd.py
@@ -1,24 +1,24 @@
 import mysql.connector
 from mysql.connector import errorcode
-import logging
 
 from secret import BDD_INFO
+from logger import rootLogger
 
 
 class fh_bdd:
   cnx = None
 
-  def __new__(self):
-    logging.debug("Initialisation de la classe "+self.__name__ )
+  def __init__(self):
+    rootLogger.debug("Initialisation de la classe fh_bdd" )
     try:
       
-      logging.debug("Connexion à la base...")
+      rootLogger.debug("Connexion à la base...")
       self.cnx = mysql.connector.connect(**BDD_INFO)
-      logging.debug("Connecté à la base...")
+      rootLogger.debug("Connecté à la base...")
     except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        logging.error("Something is wrong with your user name or password")
+        rootLogger.error("Something is wrong with your user name or password")
       elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        logging.error("Database does not exist")
+        rootLogger.error("Database does not exist")
       else:
-        logging.error(err)
+        rootLogger.error(err)
--- fun_hosp_server.py
+++ fun_hosp_server.py
@@ -3,7 +3,6 @@
 
 # dépendance python native
 import json
-import logging
 import traceback
 import pathlib
 import sys
@@ -11,6 +10,8 @@ import sys
 # dépendance au moteur API falcon
 import falcon
 from falcon_swagger_ui import register_swaggerui_app
+# dépendance de debug
+from wsgiref import simple_server
 
 # dépendance au moteur google Oauth
 from google.oauth2 import id_token
@@ -19,54 +20,59 @@ from secret import GOOGLE_API_KEY
 
 # le reste de l'appli
 from bdd import fh_bdd   # module de gestion de la base de données
+from logger import rootLogger
 
 SWAGGERUI_URL = '/swagger'
 SCHEMA_URL =  '/static/v1/swagger.yaml'
 STATIC_PATH = pathlib.Path(__file__).parent / 'static'
 
-
 page_title = 'FH_Assist Swagger Doc'
 favicon_url = 'https://funhospital.tsuna.fr/favicon.ico'
 
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
-logging.info("Démarrage de l'api")
+
+rootLogger.info("Démarrage de l'api")
+
+def handle_404(req, resp):
+    resp.status = falcon.HTTP_404
+    resp.body = 'Not found'
+
 
 class fh_login:
   token= None
   db= None
   CLIENT_ID=GOOGLE_API_KEY
 
-  def __new__(self):
-    logging.debug("Initialisation de l'api "+self.__name__ )
+  def __init__(self):
+    rootLogger.debug("Initialisation de l'api fh_login" )
     self.db = fh_bdd()
 
   def on_post(self,req, resp):
+    rootLogger.info("Appel de la commande POST de l'api fh_login")
     response={}
     # Demande de connexion à l'application
-    logging.debug("Req = "+str(req))
+    rootLogger.debug("Req = "+str(req))
     try:
       data = json.load(req.stream)
     except Exception as e:
       resp.status = falcon.HTTP_400
       return
-    logging.debug("data = "+str(data))
-    logging.debug("Token = "+str(data['token']))
+    rootLogger.debug("data = "+str(data))
     try:
       google_token= data['token']
       # validation du token google
       idinfo=id_token.verify_oauth2_token(google_token, requests.Request(), self.CLIENT_ID)
-      logging.debug("idinfo = "+str(idinfo))
+      rootLogger.debug("idinfo = "+str(idinfo))
       if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
           raise ValueError('Wrong issuer.')
       userid = idinfo['sub']
 
-      logging.debug("UserData = "+str(userid))
+      rootLogger.debug("UserData = "+str(userid))
       response['result']=u"OK"
       resp.body=json.dumps(response)
       resp.status = falcon.HTTP_200
       return
     except Exception as e:
-      logging.error(str(e))
+      rootLogger.error(str(e))
       response['result']=u"KO"
       resp.body=json.dumps(response)
       resp.status = falcon.HTTP_401
@@ -74,13 +80,13 @@ class fh_login:
 try:
   api = falcon.API()
   api.add_route("/connect",fh_login())
+  api.add_sink(handle_404, '')
+
   # Ajout du swagger de l'API
   api.add_static_route('/static', str(STATIC_PATH))
 except Exception as e:
-  logging.error(str(e))
+  rootLogger.error(str(e))
   sys.exit(1)
-
-
 
 register_swaggerui_app(
     api, SWAGGERUI_URL, SCHEMA_URL,
@@ -88,3 +94,8 @@ register_swaggerui_app(
     favicon_url=favicon_url,
     config={'supportedSubmitMethods': ['get'], }
 )
+
+if __name__ == '__main__':
+    httpd = simple_server.make_server('127.0.0.1', 8000, api)
+    httpd.serve_forever()
+
